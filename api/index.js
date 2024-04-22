df25e6162cb78390795716ab6ffaefc0cb387bff
BrigeLub <88496038+BrigeLub@users.noreply.github.com>
2024-04-22 15:33:45 -0400

    refactor: troubleshoot cleanup

--- api/index.js
+++ api/index.js
@@ -1,20 +1,16 @@
 const express = require("express");
 const app = express();
-
 const path = require("path");
-const rootDir = path.dirname(require.main.filename);
 
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
-  console.log(rootDir);
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get("/about", (req, res) => {
-  console.log(rootDir);
   res.sendFile(path.join(__dirname, "public", "html/about.html"));
 });
 
